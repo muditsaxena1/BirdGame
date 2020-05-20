437c66d740eb5035eb09a2cb976280cff00466c7
NishBrod <43043984+NishBrod@users.noreply.github.com>
2020-05-19 22:18:09 -0400

    optimized

--- Assets/_Scripts/BikeController.cs
+++ Assets/_Scripts/BikeController.cs
@@ -6,21 +6,16 @@ public class BikeController : MonoBehaviour
 {
 
     public float forceZ = 3f, forceX = 3f;
-    private float speedZ, speedX;
-    [SerializeField]
-    float maxSpeedZ = 25f, maxSpeedX = 10f, maxRotation = 0.05f;
-    public float dampingFactor = 0.99f;
-
     private Rigidbody rb;
-    private Vector3 speed;
-
     private float xInpt;
+
+    
+    private float speedZ;
+    [SerializeField]
+    float maxSpeed = 6f, maxRotation = 0.05f;
     private float rotationZ;
     [SerializeField]
     float tilt = 1f;
-    public float changeDirectionFactor = 5f;
-    [HideInInspector]
-    public bool leftButtonPressed = false, rightButtonPressed = false;
 
     // Start is called before the first frame update
     void Start()
@@ -30,79 +25,24 @@ public class BikeController : MonoBehaviour
 
     private void Update()
     {
-        if (leftButtonPressed)
-        {
-            xInpt = -1f;
-        }
-        else if (rightButtonPressed)
-        {
-            xInpt = 1f;
-        }
-        else
-        {
-            xInpt = 0f;
-        }
-        if(xInpt == 0)
-        {
-            xInpt = Input.GetAxis("Horizontal");
-        }
+        xInpt = Input.GetAxis("Horizontal");
     }
     // Update is called once per frame
     void FixedUpdate()
     {
-        //Velocities
-        if (speedZ < maxSpeedZ)
+        Vector3 forceDir = Vector3.zero;
+        speedZ = rb.velocity.z;
+        rotationZ = rb.rotation.z;
+        if (rb.velocity.z < maxSpeed)
         {
-            speedZ += forceZ * Time.deltaTime;
+            forceDir.z = forceZ * Time.deltaTime;
         }
+        forceDir.x = xInpt * forceX * Time.deltaTime;
+        print(forceDir);
+        rb.AddForce(forceDir,ForceMode.Acceleration);
 
-        // X speed
-        if (xInpt > 0 && speedX < 0)
-        {
-            //Debug.Log("CHanging dir");
-            speedX += forceX * Time.deltaTime * changeDirectionFactor;
-        }
-        else if (xInpt > 0 && speedX < maxSpeedX)
-        {
-            speedX += forceX * Time.deltaTime;
-        }
-        else if (xInpt < 0 && speedX > 0)
-        {
-            //Debug.Log("CHanging dir");
-            speedX -= forceX * Time.deltaTime * changeDirectionFactor;
-        }
-        else if(xInpt < 0 && speedX > -maxSpeedX)
-        {
-            speedX -= forceX * Time.deltaTime;
-        }
-        else if(xInpt == 0)
-        {
-            speedX = speedX * dampingFactor;
-        }
-        speed = new Vector3(speedX, 0f, speedZ);
-        rb.MovePosition(transform.position + speed * Time.deltaTime);
-
-
-        //Rotation
-        float rotation = speedX * -tilt * Time.deltaTime;
-        rotation = rotationZ = Mathf.Clamp(rotation, -maxRotation, maxRotation);
-        rb.rotation = Quaternion.Euler(0f, 0f, rotation);
-
-        
-        
-        //Vector3 forceDir = Vector3.zero;
-        //speedZ = rb.velocity.z;
-        //rotationZ = rb.rotation.z;
-        //if (rb.velocity.z < maxSpeed)
-        //{
-        //    forceDir.z = forceZ * Time.deltaTime;
-        //}
-        //forceDir.x = xInpt * forceX * Time.deltaTime;
-        //print(forceDir);
-        //rb.AddForce(forceDir,ForceMode.Acceleration);
-
-        //float zRotation = rb.velocity.x * -tilt;
-        //zRotation = Mathf.Clamp(zRotation, -maxRotation, maxRotation);
-        //rb.rotation = Quaternion.Euler(0f, 0f, zRotation);
+        float zRotation = rb.velocity.x * -tilt;
+        zRotation = Mathf.Clamp(zRotation, -maxRotation, maxRotation);
+        rb.rotation = Quaternion.Euler(0f, 0f, zRotation);
     }
 }
