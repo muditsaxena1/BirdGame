824d7ea8c6db45426f75b8ab7c45e6af402f212b
muditsaxena1 <mudit.25102010@gmail.com>
2020-05-25 13:12:40 +0530

    added obj pooling on streetlights

--- Assets/_Scripts/ObjectPooler.cs
+++ Assets/_Scripts/ObjectPooler.cs
@@ -6,22 +6,19 @@ using MarchingBytes;
 public class ObjectPooler : MonoBehaviour
 {
     EasyObjectPool easyObjectPool;
-    private Transform leftBuildings, rightBuildings, roads;
-    private float nextRoadPos = 120f;
+    public Transform leftBuildings, rightBuildings, roads, streetlights;
+    private float nextRoadPos = 120f, nextStreetlightPos = 40f;
     private float nextRightHousePos = 40f, nextLeftHousePos = 20f;
     public int[] houseLength;
 
-    Queue<GameObject> roadQ, leftBuildingsQ, rightBuildingsQ;
+    Queue<GameObject> roadQ, leftBuildingsQ, rightBuildingsQ, streetlightsQ;
 
     private void Start()
     {
         roadQ = new Queue<GameObject>();
         leftBuildingsQ = new Queue<GameObject>();
         rightBuildingsQ = new Queue<GameObject>();
-
-        leftBuildings = GameObject.Find("LeftBuildings").transform;
-        rightBuildings = GameObject.Find("RightBuildings").transform;
-        roads = GameObject.Find("Roads").transform;
+        streetlightsQ = new Queue<GameObject>();
     }
     // Update is called once per frame
     void FixedUpdate()
@@ -30,6 +27,14 @@ public class ObjectPooler : MonoBehaviour
         {
             easyObjectPool = EasyObjectPool.instance;
         }
+        if(streetlightsQ.Count > 0)
+        {
+            if(transform.position.z > streetlightsQ.Peek().transform.position.z)
+            {
+                easyObjectPool.ReturnObjectToPool(streetlightsQ.Dequeue());
+            }
+        }
+
         if (roadQ.Count > 0)
         {
             if (transform.position.z + 80f > roadQ.Peek().transform.position.z)
@@ -39,14 +44,14 @@ public class ObjectPooler : MonoBehaviour
         }
         if (leftBuildingsQ.Count > 0)
         {
-            if (transform.position.z > leftBuildingsQ.Peek().transform.position.z)
+            if (transform.position.z - 10f > leftBuildingsQ.Peek().transform.position.z)
             {
                 easyObjectPool.ReturnObjectToPool(leftBuildingsQ.Dequeue());
             }
         }
         if (rightBuildingsQ.Count > 0)
         {
-            if (transform.position.z > rightBuildingsQ.Peek().transform.position.z)
+            if (transform.position.z - 60f > rightBuildingsQ.Peek().transform.position.z)
             {
                 easyObjectPool.ReturnObjectToPool(rightBuildingsQ.Dequeue());
             }
@@ -77,6 +82,20 @@ public class ObjectPooler : MonoBehaviour
             house.transform.parent = leftBuildings;
             leftBuildingsQ.Enqueue(house);
             //print(index);
+        }
+        if(transform.position.z + 180f > nextStreetlightPos)
+        {
+            Vector3 pos = new Vector3(-19f,91.5f, nextStreetlightPos);
+            GameObject slight = easyObjectPool.GetObjectFromPool("Streetlight", pos, Quaternion.identity);
+            slight.transform.parent = streetlights;
+            streetlightsQ.Enqueue(slight);
+
+            pos = new Vector3(-46.8f, 91.5f, nextStreetlightPos);
+            slight = easyObjectPool.GetObjectFromPool("Streetlight", pos, Quaternion.Euler(0f, 180f, 0f));
+            slight.transform.parent = streetlights;
+            streetlightsQ.Enqueue(slight);
+
+            nextStreetlightPos += 40f;
         }
     }
 }
