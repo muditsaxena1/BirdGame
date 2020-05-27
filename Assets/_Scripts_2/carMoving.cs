888f6385c7281a047c286c1cf5ce34ad1f9437b7
NishBrod <43043984+NishBrod@users.noreply.github.com>
2020-05-26 20:59:39 -0400

    some cars added

---
+++ Assets/_Scripts_2/carMoving.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carMoving : MonoBehaviour
+{
+    [SerializeField]
+    public float speed;
+    public GameObject[] fastCars;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fastCars = GameObject.FindGameObjectsWithTag("Fast_Cars");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject fastCar in fastCars) {
+            fastCar.transform.position += fastCar.transform.forward * Time.deltaTime *
+                speed; }
+    }
+}
---
+++ Assets/_Scripts_2/slowCarMoving.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slowCarMoving : MonoBehaviour
+{
+    [SerializeField]
+    public float speed;
+    public GameObject[] slowCars;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slowCars = GameObject.FindGameObjectsWithTag("Slow_Cars");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject slowCar in slowCars)
+        {
+            slowCar.transform.position += slowCar.transform.forward * Time.deltaTime *
+                speed;
+        }
+    }
+}
