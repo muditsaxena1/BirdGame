a97f7f5db01603944fe70b7dab2746b09770c2c9
NishBrod <43043984+NishBrod@users.noreply.github.com>
2020-06-07 02:19:52 -0400

    coin counter

---
+++ Assets/_Scripts_2/CoinCollector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinCollector : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    
+    void Update()
+    {
+        
+        
+    }
+
+         void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Bike")
+            {
+            PlayerManager.noc += 1;
+            Debug.Log("Coins:"+PlayerManager.noc);
+                Destroy(gameObject);
+              
+        }
+        }
+    
+}
---
+++ Assets/_Scripts_2/PlayerManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerManager : MonoBehaviour
+{
+    public static int noc;
+    public Text coinText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        noc = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coinText.text = "Coins:" + noc;
+    }
+}
