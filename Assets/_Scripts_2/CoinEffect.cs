ad444ffd44190dd339b5d42b10f5aacea6d724dd
NishBrod <43043984+NishBrod@users.noreply.github.com>
2020-06-11 04:22:09 -0400

    coin sound

--- Assets/_Scripts_2/CoinEffect.cs
+++ Assets/_Scripts_2/CoinEffect.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class CoinEffect : MonoBehaviour
 {
     public GameObject effect;
+    private AudioSource coinSource;
     // Start is called before the first frame update
     void Start()
     {
-        
+        coinSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -21,6 +22,7 @@ public class CoinEffect : MonoBehaviour
     {
         if (other.tag == "Bike")
         {
+            coinSource.Play();
             Instantiate(effect,transform.position,transform.rotation);
         }
     }
