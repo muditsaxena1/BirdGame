3d75bd5832c49ec261ccccf4e9409977752e140d
NishBrod <43043984+NishBrod@users.noreply.github.com>
2020-06-12 04:57:57 -0400

    coin magnet

---
+++ Assets/_Scripts_2/coin.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour
+{
+    public Component[] meshes;
+    public GameObject effect;
+    private AudioSource coinSource;
+
+    void Start()
+    {
+        coinSource = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Update()
+    {
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bike")
+        {
+            Instantiate(effect, transform.position, transform.rotation);
+            meshes = GetComponentsInChildren<MeshRenderer>();
+
+            PlayerManager.noc += 1;
+            Debug.Log("Coins:" + PlayerManager.noc);
+            foreach(MeshRenderer mesh in meshes)
+            {
+                mesh.enabled = false;
+            }
+            coinSource.Play();
+            //Instantiate(effect, transform.position, transform.rotation);
+
+
+        }
+    }
+}
